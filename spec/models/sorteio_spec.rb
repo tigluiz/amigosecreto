b069f305970a975d346ceeecab5e5ec635445f23
Tiago Luiz de Almeida <tig.luiz.almeida@gmail.com>
2009-12-22 13:34:39 -0200

    modificando o sorteio_spec

--- spec/models/sorteio_spec.rb
+++ spec/models/sorteio_spec.rb
@@ -1,12 +1,17 @@
 require 'spec_helper'
 
 describe Sorteio do
-  before(:each) do
-    @valid_attributes = {
-      
-    }
-  end
-
+ it "deve sortear um amigo secreto para um participante" do
+   pending
+ end
+ it "nao deve sortear a si proprio" do
+   pending
+ end
+  
+ it "nao deve ter o mesmo amigo secreto para mais de um participante" do
+   pending
+ end
+  
   it "should create a new instance given valid attributes" do
     Sorteio.create!(@valid_attributes)
   end
