16666974bae60ad360ef8955f64a2d5b5db76049
Tiago Luiz de Almeida <tig.luiz.almeida@gmail.com>
2009-12-24 21:29:14 -0200

    refactoring

--- app/controllers/participantes_controller.rb
+++ app/controllers/participantes_controller.rb
@@ -1,10 +1,4 @@
 class ParticipantesController < InheritedResources::Base
   belongs_to :sorteio
   respond_to :html, :js   
-  
-  def amigo_secreto
-    @sorteio = Sorteio.find params["id"]
-    @sorteio.amigo_secreto
-    render :index
-  end
 end
